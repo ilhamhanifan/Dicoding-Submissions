fcd26eb7097462e65b25dc233b72789cd9b02d76
Muhammad Ilham Hanifan <the.ilhamhanifan@gmail.com>
2022-07-16 17:29:15 +0700

    add Menjadi Cloud Engineer

---
+++ Menjadi_Cloud_Engineer/Project 1 - Portofolio on App Engine/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3003
+
+app.use(express.static(__dirname + '/www'))
+
+app.get('/', (req,res) => {
+  res.sendFile(path.resolve(__dirname, '/www/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`App running on ${port}`)
+})
